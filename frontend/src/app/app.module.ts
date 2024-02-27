296111cebc8e6bfe766c376bcf30e2e5f215074a
Rodrigo Santos <rodrigom376@gmail.com>
2024-02-27 20:24:14 -0300

    instalação bootstrap e criação de componentes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './components/nav/nav.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ClientesComponent,
+    ProdutosComponent,
+    PedidosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
