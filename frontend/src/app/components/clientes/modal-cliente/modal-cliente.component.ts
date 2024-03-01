9c13ae641d3add12155dfb8bcb8557082c5a3c09
Rodrigo Santos <rodrigom376@gmail.com>
2024-02-29 22:21:24 -0300

    submit modal

--- frontend/src/app/components/clientes/modal-cliente/modal-cliente.component.ts
+++ frontend/src/app/components/clientes/modal-cliente/modal-cliente.component.ts
@@ -1,18 +1,30 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-modal-cliente',
   templateUrl: './modal-cliente.component.html',
-  styleUrl: './modal-cliente.component.scss'
+  styleUrls: ['./modal-cliente.component.scss']
 })
 export class ModalClienteComponent {
-  constructor(
-    public dialogRef: MatDialogRef<ModalClienteComponent>, @Inject(MAT_DIALOG_DATA) public data: any
+  selectedCategoria: string = '';
+  mostrar: boolean = false;
+  usuario: any = {};
 
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any
   ) { }
 
-  onClose(): void {
-    this.dialogRef.close();
+  adicionarUsuario(formValue: any): void {
+    this.usuario.nome = formValue.nome;
+    this.usuario.preco = formValue.preco;
+    this.usuario.estoque = formValue.estoque;
+    this.usuario.validade = formValue.validade;
+    this.usuario.perecivel = formValue.perecivel === 'Sim';
+
+    console.log('Novo usuario adicionado:', this.usuario);
+
+    // Remova esta linha para evitar que a modal seja fechada automaticamente
+    // this.dialogRef.close();
   }
 }
