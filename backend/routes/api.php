3898dfa482b354f1432616d09d917024b17762c8
Rodrigo Santos <rodrigom376@gmail.com>
2024-03-02 10:25:52 -0300

    rotas pedidos

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\ClienteController;
+use App\Http\Controllers\API\PedidosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/cliente', [ClienteController::class, 'index'])->name('index');
@@ -8,3 +9,10 @@ Route::post('/cliente', [ClienteController::class, 'store'])->name('store');
 Route::delete('/cliente/delete/{id}', [ClienteController::class, 'destroy'])->name('destroy');
 Route::get('/cliente/{id}', [ClienteController::class, 'show'])->name('show');
 Route::put('/cliente/{id}', [ClienteController::class, 'update'])->name('update');
+
+
+Route::get('/pedidos', [PedidosController::class, 'index'])->name('index');
+Route::post('/pedidos', [PedidosController::class, 'store'])->name('store');
+Route::delete('/pedidos/delete/{id}', [PedidosController::class, 'destroy'])->name('destroy');
+Route::get('/pedidos/{id}', [PedidosController::class, 'show'])->name('show');
+Route::put('/pedidos/{id}', [PedidosController::class, 'update'])->name('update');
